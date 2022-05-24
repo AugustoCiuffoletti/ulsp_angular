ecbb7943c3c67358cabb8d0b4eb8d6ee70793f0f
Augusto Ciuffoletti <augusto.ciuffoletti@gmail.com>
2022-05-24 17:31:15 +0200

    Prima mappa!

--- src/app/components/home-content/tools/colleziona/colleziona.component.ts
+++ src/app/components/home-content/tools/colleziona/colleziona.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-colleziona',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CollezionaComponent implements OnInit {
 
+  centerLat = 46.067;
+  centerLng = 11.121;
+  zoom = 10;
+  
   constructor() { }
 
   ngOnInit(): void {
+	var aMap = L.map('mapid', {
+      center: L.latLng(this.centerLat, this.centerLng),
+      zoom: this.zoom,
+      zoomControl: false,
+      dragging: false,
+      scrollWheelZoom: false,
+      layers: [L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')],
+    });
   }
 
 }
